563418cead37bcd05a8928f976ba29f2324b68e1
Chandrima Hazra <hazrachandrima6@gmail.com>
2024-11-05 01:49:57 +0530

    improvisation

--- toDo/toDo.ps1
+++ toDo/toDo.ps1
@@ -1,22 +1,46 @@
 # a script that maintains a silly toDo list, nothing more, nothing less!
+# Written by: Chandrima Hazra
 
 $Host.UI.RawUI.BackgroundColor = "Black"
 $Host.UI.RawUI.ForegroundColor = "White"
 
 Write-Host "`n---- Welcome! -----" -ForegroundColor "Cyan"
 
-$tasks = New-Object System.Collections.ArrayList
+if (!(Test-Path -Path $PSScriptRoot)) {
+    New-Item -ItemType Directory -Path $PSScriptRoot
+}
+
+$listPath = "$PSScriptRoot\tasks.json"
+
+if(Test-Path -Path $listPath) {
+	$tasks = [System.Collections.ArrayList](Get-Content -Path $listPath | ConvertFrom-Json)
+} else {
+	$tasks = New-Object System.Collections.ArrayList
+}
+
+
+#saves the to-Do tasks in file tasks.json in the same directory as the script
+function saveList() {
+	$tasks | ConvertTo-Json | Set-Content -Path $listPath
+}
+
 
 function toDo() {
 	
-	Write-Host "`nEnter:`tview`n`tadd`n`tdelete`n`texit" -ForegroundColor "DarkYellow"
+	Write-Host "`nEnter:`t'view' to view your to-do list`n`t'add' to add a new task`n`t'delete' to delete a task`n`t'clear' to clear the entire list`n`t'exit' to exit" -ForegroundColor "DarkYellow"
 	$choice = Read-Host 
 
 	if($choice -eq "exit") {
 		Write-Host "`nExited.`n" -ForegroundColor "Cyan"
+		saveList
 		exit
 	} else {
-		if($choice -eq "view") {
+		if($choice -eq "clear") {
+			$tasks = New-Object System.Collections.ArrayList
+			Remove-Item '.\tasks.json'
+			Write-Host "`nto-do list cleared successfully!" -ForegroundColor "Green"
+		}
+		elseif($choice -eq "view") {
 			if($tasks.Count -eq 0) {
 				Write-Host "`nNo tasks remaining" -ForegroundColor "DarkCyan"
 			} else {
